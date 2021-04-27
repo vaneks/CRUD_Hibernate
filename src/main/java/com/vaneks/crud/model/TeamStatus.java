4231936eac42734609ff7e8ea612f436afb976bb
Ivan <35092751+vaneks@users.noreply.github.com>
2021-04-27 20:57:46 +0800

    Add files via upload

---
+++ src/main/java/com/vaneks/crud/model/TeamStatus.java
@@ -0,0 +1,8 @@
+package com.vaneks.crud.model;
+
+public enum TeamStatus {
+    ACTIVE, UNDER_REVIEW, DELETED;
+
+    TeamStatus() {
+    }
+}
---
+++ src/main/java/com/vaneks/crud/repository/Hibernate/HibernateSkillRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.vaneks.crud.repository.Hibernate;
+
+import com.vaneks.crud.model.Skill;
+import com.vaneks.crud.repository.SkillRepository;
+import com.vaneks.crud.utils.HibernateSession;
+import org.hibernate.Session;
+import java.util.List;
+
+public class HibernateSkillRepositoryImpl implements SkillRepository {
+
+    @Override
+    public List<Skill> getAll() {
+        Session session = HibernateSession.getSession();
+        session.beginTransaction();
+        List<Skill> skills = session.createQuery("FROM Skill").list();
+        session.getTransaction().commit();
+        return skills;
+    }
+
+    @Override
+    public Skill getById(Long id) {
+        Session session = HibernateSession.getSession();
+        session.beginTransaction();
+        Skill skill = (Skill) session.get(Skill.class, id);
+        session.getTransaction().commit();
+        return skill;
+    }
+
+    @Override
+    public Skill save(Skill skill) {
+        Session session = HibernateSession.getSession();
+        session.beginTransaction();
+        session.save(skill);
+        session.getTransaction().commit();
+        return skill;
+    }
+
+    @Override
+    public Skill update(Skill skill) {
+        Session session = HibernateSession.getSession();
+        session.beginTransaction();
+        session.update(skill);
+        session.getTransaction().commit();
+        return skill;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Session session = HibernateSession.getSession();
+        session.beginTransaction();
+        Skill skill = (Skill) session.get(Skill.class, id);
+        session.delete(skill);
+        session.getTransaction().commit();
+    }
+}
